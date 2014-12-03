90cde1b4ee8282af59e51ee7e79e956f106d0a1f
rioll <salemioche92@hotmail.fr>
2014-12-03 16:02:20 +0100

    modif news js + html

--- scripts/controllers/news.js
+++ scripts/controllers/news.js
@@ -8,11 +8,18 @@
  * Controller of the applinoobApp
  */
 var app = angular.module('applinoobApp');
-  app.controller('NewsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  app.controller('NewsCtrl', function () {
+    
+	this.tab=1;
+	this.selectTab=function(setTab){
+		this.tab=setTab;
+	};
+	
+	this.isSelected = function(checkTab){
+	return this.tab === checkTab;
+		
+	};
+	
+	
 
   });
