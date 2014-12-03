73af90fe8466ea2f40eadb960176c97f9a76db21
lfilleur <l.filleur@gmail.com>
2014-12-03 23:09:22 +0100

    Rectification des erreurs made in Github
    Rectification des erreurs made in Github

--- scripts/controllers/news.js
+++ scripts/controllers/news.js
@@ -8,23 +8,8 @@
  * Controller of the applinoobApp
  */
 var app = angular.module('applinoobApp');
-<<<<<<< HEAD
+
   app.controller('NewsCtrl', function ($scope) {
     $scope.pageClass = 'news';
-=======
-  app.controller('NewsCtrl', function () {
-    
-	this.tab=1;
-	this.selectTab=function(setTab){
-		this.tab=setTab;
-	};
-	
-	this.isSelected = function(checkTab){
-	return this.tab === checkTab;
-		
-	};
-	
-	
 
->>>>>>> origin/master
   });
