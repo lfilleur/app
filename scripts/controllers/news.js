2d8881e2785772d086aabd6554dbdbe4ef1164de
lfilleur <l.filleur@gmail.com>
2014-12-04 09:59:06 +0100

    Rectifications derniers bugs news
    Rectifications derniers bugs news

--- scripts/controllers/news.js
+++ scripts/controllers/news.js
@@ -1,15 +1,24 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name applinoobApp.controller:NewsCtrl
- * @description
- * # NewsCtrl
+/** @@ -8,6 +8,23 @@
  * Controller of the applinoobApp
  */
+
 var app = angular.module('applinoobApp');
 
-  app.controller('NewsCtrl', function ($scope) {
-    $scope.pageClass = 'news';
 
+  app.controller('NewsCtrl', function () {
+      
+      
+    
+	this.tab=1;
+	this.selectTab=function(setTab){
+		this.tab=setTab;
+	};
+	
+	this.isSelected = function(checkTab){
+	return this.tab === checkTab;
+		
+	};
+	
   });
