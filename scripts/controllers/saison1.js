dbb78f2d846cadeb926f4779c5c01ceb1007f2a2
rioll <salemioche92@hotmail.fr>
2014-12-11 16:09:00 +0100

    Mise en place video saison 1

--- scripts/controllers/saison1.js
+++ scripts/controllers/saison1.js
@@ -8,22 +8,31 @@
  * Controller of the Saison1Ctrl
  */
 var app = angular.module('saison1');
-  app.controller('Saison1Ctrl', function ($scope) {
-    $scope.items = [
-    { id: 1, nom: 'Episode 1', saison: 'Saison 1',episode:'Episode1', image:'images/555x125.gif', synopsis:'aaa', url:'//www.youtube.com/embed/Tv9H1vv-Qrs'},
-    {  id: 2, nom: 'Episode 2', saison: 'Saison 1',episode:'Episode2', image:'images/555x125.gif', synopsis:'aaa', url:'//www.youtube.com/embed/Tv9H1vv-Qrs'},
-     { id: 3, nom: 'Episode 3', saison: 'Saison 1',episode:'Episode3', image:'images/555x125.gif', synopsis:'aaa', url:'//www.youtube.com/embed/Tv9H1vv-Qrs'},
-     {id: 4, nom: 'Episode 1', saison: 'Saison 2',episode:'Episode4', image:'images/555x125.gif', synopsis:'aaa', url:'//www.youtube.com/embed/Tv9H1vv-Qrs'}
-  ];
-  	$scope.pageClass = 'saison1';
-  
+   app.controller('Saison1Ctrl', function($scope, $http) {
 
-  } /*,function urlVideo($scope){
+      var url = 'http://smartsheep.reaco.fr/php/listeSaison1.php';
+     
+      $http.get(url).success(httpSuccess).error(function(){
+
+         alert('Impossible de trouvé la saison 1');
+
+      });
+
+       function httpSuccess (response){
+        
+         $scope.listeSaison1 = response;
+         
+      }
+
+  });
+
+
+  /*,function urlVideo($scope){
 
 	var $url = $scope.items.url;
  	return '<iframe width="560" height="315" src="'+ $url +'" frameborder="0" allowfullscreen></iframe>';
 
- }*/);
+ }
 
    
 
@@ -36,7 +45,7 @@ app.directive('Saison1Dir', function($scope) {
   
 });
 
-
+*/
   
   
   
